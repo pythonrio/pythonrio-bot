0d1629fa9ef2c8237eeea5c2d9202ce4872659f8
Raysa Dutra <hi-hi-ray@users.noreply.github.com>
2016-08-08 20:41:49 -0300

    Update PythonRio-bot-all.rb

--- PythonRio-bot-all.rb
+++ PythonRio-bot-all.rb
@@ -8,20 +8,20 @@ token = '239201720:AAFA16ppUfKeuwtuwr2JjriHNRgDcEipdbw'
 Telegram::Bot::Client.run(token) do |bot|
 	bot.listen do |message|
 		case message.text
-		when '/start'
-			bot.api.send_message(chat_id: message.chat.id, text: "Força Python!")
-		when '/start@PythonRio_Bot'
-			bot.api.send_message(chat_id: message.chat.id, text: "Força Python!")		
-		when '/evento'
-			bot.api.send_message(chat_id: message.chat.id, text: "Prezados,
+			when '/start'
+				bot.api.send_message(chat_id: message.chat.id, text: "Força Python!")
+			when '/start@PythonRio_Bot'
+				bot.api.send_message(chat_id: message.chat.id, text: "Força Python!")		
+			when '/evento'
+				bot.api.send_message(chat_id: message.chat.id, text: "Prezados,
 				Dia 27 de Agosto de 2016 vai rolar XII Encontro da @pythonrio. Submeta sua proposta de palestra até dia 20/08/2016 pelo speakerfight http://speakerfight.com/events/xii-encontro-python-rio/
 				Ainda estamos definindo o local e, assim que soubermos, liberamos em nossa Fan Page os links para a inscrição.
 
 				Grande abraço!
 				#forcapython")
 		
-when '/evento@PythonRio_Bot'
-			bot.api.send_message(chat_id: message.chat.id, text: "Prezados,
+			when '/evento@PythonRio_Bot'
+				bot.api.send_message(chat_id: message.chat.id, text: "Prezados,
 				Dia 27 de Agosto de 2016 vai rolar XII Encontro da @pythonrio. Submeta sua proposta de palestra até dia 20/08/2016 pelo speakerfight http://speakerfight.com/events/xii-encontro-python-rio/
 				Ainda estamos definindo o local e, assim que soubermos, liberamos em nossa Fan Page os links para a inscrição.
 
@@ -29,16 +29,16 @@ when '/evento@PythonRio_Bot'
 				#forcapython")
 
 
-when '/regras'
-			bot.api.send_message(chat_id: message.chat.id, text: "Não farás Pyratarias. Temos um clube do livro, confira em link.
+			when '/regras'
+				bot.api.send_message(chat_id: message.chat.id, text: "Não farás Pyratarias. Temos um clube do livro, confira em link.
 				Não ofenderás o Coleguinha, Não aceitamos discriminação de qualquer tipo.
 				Não mandarás nudes, Somos pythonistas só nos excitamos com código.
 				Nao mandarás porno, mesmo que the internet is for porn
 				Não me invocarás de 1 em 1 minuto.
 				Falarás do PythonRio para os coleguinhas.")
 
-when '/regras@PythonRio_Bot'
-			bot.api.send_message(chat_id: message.chat.id, text: "Não farás Pyratarias. Temos um clube do livro, confira em link.
+			when '/regras@PythonRio_Bot'
+				bot.api.send_message(chat_id: message.chat.id, text: "Não farás Pyratarias. Temos um clube do livro, confira em link.
 				Não ofenderás o Coleguinha, Não aceitamos discriminação de qualquer tipo.
 				Não mandarás nudes, Somos pythonistas só nos excitamos com código.
 				Nao mandarás porno, mesmo que the internet is for porn.
@@ -62,7 +62,7 @@ when '/regras@PythonRio_Bot'
 					YouTube = https://www.youtube.com/channel/UCGl4xDZYUYGg4_aMah32yjQ
 
 					Clube do Livro = https://trello.com/invite/b/k75OyRBC/1e822b1c6b177f4cc501c39aa9a64e44/clube-do-livro-pythonrio")
-when '/help@PythonRio_Bot'
+			when '/help@PythonRio_Bot'
 				bot.api.send_message(chat_id: message.chat.id, text: "Olá! A forma mais correta de me usar a / sem o @PythonRio_Bot:
 					/start - Digo Força Python!
 					/evento - Situação Atual do evento.
@@ -80,6 +80,6 @@ when '/help@PythonRio_Bot'
 					YouTube = https://www.youtube.com/channel/UCGl4xDZYUYGg4_aMah32yjQ
 
 					Clube do Livro = https://trello.com/invite/b/k75OyRBC/1e822b1c6b177f4cc501c39aa9a64e44/clube-do-livro-pythonrio")
-				end
-			end
 		end
+	end
+end
